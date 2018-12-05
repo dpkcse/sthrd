34e19367830133bced3b33ecc06e294aed66b12d
dpkcse <dpkcse@users.noreply.github.com>
2018-12-06 01:59:14 +0600

    1St

---
+++ application/views/partials/foot.php
@@ -0,0 +1,21 @@
+<!-- javascript
+    ================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="<?php echo base_url(); ?>assets/js/jquery.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/jquery.easing.1.3.js"></script>
+<script src="<?php echo base_url(); ?>assets/materialize/js/materialize.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/jquery.fancybox.pack.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/jquery.fancybox-media.js"></script>  
+<script src="<?php echo base_url(); ?>assets/js/jquery.flexslider.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/animate.js"></script>
+<!-- Vendor Scripts -->
+<script src="<?php echo base_url(); ?>assets/js/modernizr.custom.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/jquery.isotope.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/jquery.magnific-popup.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/animate.js"></script> 
+<script src="<?php echo base_url(); ?>assets/js/custom.js"></script>
+</body>
+
+<!-- Mirrored from webthemez.com/demo/xtream-corporate-responsive-website-template/ by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 05 Dec 2018 16:44:30 GMT -->
+</html>
---
+++ application/views/partials/head.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- Mirrored from webthemez.com/demo/xtream-corporate-responsive-website-template/ by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 05 Dec 2018 16:44:14 GMT -->
+<head>
+<meta charset="utf-8">
+<title>Xtream Multi purpose HTML5 Template</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta name="description" content="" />
+<meta name="author" content="http://webthemez.com/" />
+<!-- css --> 
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<link rel="stylesheet" href="<?php echo base_url(); ?>assets/materialize/css/materialize.min.css" media="screen,projection" />
+<link href="<?php echo base_url(); ?>assets/css/bootstrap.min.css" rel="stylesheet" />
+<link href="<?php echo base_url(); ?>assets/css/fancybox/jquery.fancybox.css" rel="stylesheet"> 
+<link href="<?php echo base_url(); ?>assets/css/flexslider.css" rel="stylesheet" /> 
+<link href="<?php echo base_url(); ?>assets/css/style.css" rel="stylesheet" />
+ 
+<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+<!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+</head>
+<body>
