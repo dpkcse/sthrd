954fda5783cc76ca3b7028702acd854fc185f0d6
dpkcse <dipokchakraborty@gmail.com>
2018-12-09 12:48:29 +0600

    Update

--- application/views/partials/head.php
+++ application/views/partials/head.php
@@ -4,8 +4,27 @@
 <meta charset="utf-8">
 <title>Singapore Training & HR Development Pte. Limited</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-<meta name="description" content="" />
+<meta name="description" content="Singapore Training & HR Development (STHRD) is a human resource development and capacity building organization that provides multi-sectorial training in Singapore for foreign clients, STHRD organizes ex periential exchange visits and study tours in Singapore for foreign trainees. Singapore Training & HR Development (STHRD) organizes the training program in a professional manner." />
 <meta name="author" content="Singapore Training & HR Development Pte. Limited" />
+<meta name="og:title" property="og:title" content="Singapore Training & HR Development (STHRD) is a human resource development and capacity building organization that provides multi-sectorial training in Singapore for foreign clients, STHRD organizes ex periential exchange visits and study tours in Singapore for foreign trainees. Singapore Training & HR Development (STHRD) organizes the training program in a professional manner.">
+<meta name="robots" content="index, follow">
+<link rel="apple-touch-icon" sizes="57x57" href="<?php echo base_url(); ?>assets/favico/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="<?php echo base_url(); ?>assets/favico/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="<?php echo base_url(); ?>assets/favico/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="<?php echo base_url(); ?>assets/favico/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="<?php echo base_url(); ?>assets/favico/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="<?php echo base_url(); ?>assets/favico/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="<?php echo base_url(); ?>assets/favico/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="<?php echo base_url(); ?>assets/favico/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="<?php echo base_url(); ?>assets/favico/apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="<?php echo base_url(); ?>assets/favico/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="<?php echo base_url(); ?>assets/favico/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="<?php echo base_url(); ?>assets/favico/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="<?php echo base_url(); ?>assets/favico/favicon-16x16.png">
+<link rel="manifest" href="<?php echo base_url(); ?>assets/favico/manifest.json">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="msapplication-TileImage" content="<?php echo base_url(); ?>assets/favico/ms-icon-144x144.png">
+<meta name="theme-color" content="#ffffff">
 <!-- css --> 
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 <link rel="stylesheet" href="<?php echo base_url(); ?>assets/materialize/css/materialize.min.css" media="screen,projection" />
