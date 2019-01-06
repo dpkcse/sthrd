b73beed787687f7dff53bba9a6ebcf4b8db03151
Dipok Chakraborty <dipokchakraborty@gmail.com>
2019-01-06 20:49:05 +0600

    Update

--- application/config/config.php
+++ application/config/config.php
@@ -382,12 +382,15 @@ $config['encryption_key'] = '';
 | except for 'cookie_prefix' and 'cookie_httponly', which are ignored here.
 |
 */
-$config['sess_driver'] = 'files';
-$config['sess_cookie_name'] = 'ci_session';
-$config['sess_expiration'] = 7200;
-$config['sess_save_path'] = NULL;
-$config['sess_match_ip'] = FALSE;
-$config['sess_time_to_update'] = 300;
+$config['sess_driver'] 			= 'database';
+$config['sess_cookie_name']		= 'ci_sessions';
+$config['sess_expiration']		= 7200;
+$config['sess_save_path'] 		= 'ci_sessions';
+$config['sess_expire_on_close']	= TRUE;
+$config['sess_encrypt_cookie']	= FALSE;
+$config['sess_match_ip']		= FALSE;
+$config['sess_match_useragent']	= TRUE;
+$config['sess_time_to_update']	= 1800;
 $config['sess_regenerate_destroy'] = FALSE;
 
 /*
