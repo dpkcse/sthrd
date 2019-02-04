dbadd75c85bca2727ead593c0aa108c66749d931
Dipok Chakraborty <dpkcse@users.noreply.github.com>
2019-02-05 00:07:25 +0600

    Update vision.php

--- application/views/vision.php
+++ application/views/vision.php
@@ -24,7 +24,7 @@
 					</div>
 					<div class="col-md-6">
 						<div class="about-text">
-							<h3><span class="color">Vision</span></h3>
+							<h3><span class="color">Our Vision</span></h3>
 							<?php if(COUNT($program) > 0){ ?>
 								<p><?php echo $program[0]->mnv_vision; ?></p>
 							<?php } ?>
